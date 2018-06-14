c25a0c62c8286d3c66f7cb064559449c369129c2
Oliver Ruebenkoenig <ruebenko@wolfram.com>
2018-06-14 14:54:54 +0200

    Updates for the release 1.2

--- PacletInfo.m
+++ PacletInfo.m
@@ -6,7 +6,7 @@
 
 Paclet[
 Name -> "FEMAddOns",
-Version -> "1.1.1",
+Version -> "1.2",
 Description -> "Package provides additional finite element method functionality.",
 MathematicaVersion -> "11+",
 Extensions -> {
