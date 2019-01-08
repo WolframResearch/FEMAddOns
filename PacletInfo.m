4a07b2e7b71d69badd489ffc256d0a764bdde4fd
Oliver Ruebenkoenig <ruebenko@wolfram.com>
2019-01-08 14:52:04 +0100

    Infrastructure for Applications and first Application example.

--- PacletInfo.m
+++ PacletInfo.m
@@ -6,15 +6,17 @@
 
 Paclet[
 Name -> "FEMAddOns",
-Version -> "1.2",
+Version -> "1.3",
 Description -> "Package provides additional finite element method functionality.",
 MathematicaVersion -> "11+",
 Extensions -> {
 	{"Kernel", Context -> "FEMAddOns`"},
+	{"Kernel", Root -> "Applications/Kernel", Context -> {"Applications`"}},
 	{"Kernel", Root -> "DistMesh/Kernel", Context -> {"DistMesh`"}},
 	{"Kernel", Root -> "DomainDecomposition/Kernel", Context -> {"DomainDecomposition`"}},
 	{"Kernel", Root -> "FEMUtils/Kernel", Context -> {"FEMUtils`"}},
 
+	{"Documentation", Root -> "Applications/Documentation"},
 	{"Documentation", Root -> "DistMesh/Documentation"},
 	{"Documentation", Root -> "DomainDecomposition/Documentation"},
 	{"Documentation", Root -> "FEMUtils/Documentation", MainPage -> "Guides/FEMAddOns"}
