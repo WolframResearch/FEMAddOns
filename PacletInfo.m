4fb633151a34e8dd5c167834cd7ae40db1d89b9f
Oliver Ruebenkoenig <ruebenko@wolfram.com>
2019-03-14 07:07:57 +0100

    Revert to paclert version 1.3
    That is the correct next paclet number which was already set.

--- PacletInfo.m
+++ PacletInfo.m
@@ -1,26 +1,32 @@
-(* ::Package:: *)
-
 (* Paclet Info File *)
 
-(* created 2017/11/6*)
+(* created 2019/03/14*)
 
 Paclet[
-Name -> "FEMAddOns",
-Version -> "1.3.1",
-Description -> "Package provides additional finite element method functionality.",
-MathematicaVersion -> "11+",
-Extensions -> {
-	{"Kernel", Context -> "FEMAddOns`"},
-	{"Kernel", Root -> "Applications/Kernel", Context -> {"Applications`"}},
-	{"Kernel", Root -> "DistMesh/Kernel", Context -> {"DistMesh`"}},
-	{"Kernel", Root -> "DomainDecomposition/Kernel", Context -> {"DomainDecomposition`"}},
-	{"Kernel", Root -> "FEMUtils/Kernel", Context -> {"FEMUtils`"}},
-
-	{"Documentation", Root -> "Applications/Documentation"},
-	{"Documentation", Root -> "DistMesh/Documentation"},
-	{"Documentation", Root -> "DomainDecomposition/Documentation"},
-	{"Documentation", Root -> "FEMUtils/Documentation", MainPage -> "Guides/FEMAddOns"}
-}
+    Name -> "FEMAddOns",
+    Version -> "1.3",
+    MathematicaVersion -> "11+",
+    Description -> "Package provides additional finite element method functionality.",
+    Extensions -> 
+        {
+            {"Kernel", Context -> "FEMAddOns`"}, 
+            {"Kernel", Root -> "Applications/Kernel", Context -> 
+                {"Applications`"}
+            }, 
+            {"Kernel", Root -> "DistMesh/Kernel", Context -> 
+                {"DistMesh`"}
+            }, 
+            {"Kernel", Root -> "DomainDecomposition/Kernel", Context -> 
+                {"DomainDecomposition`"}
+            }, 
+            {"Kernel", Root -> "FEMUtils/Kernel", Context -> 
+                {"FEMUtils`"}
+            }, 
+            {"Documentation", Root -> "Applications/Documentation"}, 
+            {"Documentation", Root -> "DistMesh/Documentation"}, 
+            {"Documentation", Root -> "DomainDecomposition/Documentation"}, 
+            {"Documentation", Root -> "FEMUtils/Documentation", MainPage -> "Guides/FEMAddOns"}
+        }
 ]
 
 
