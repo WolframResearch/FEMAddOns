9346d7c02ea85590ff0f01b1a014116930b12e1c
Oliver Ruebenkoenig <ruebenko@wolfram.com>
2020-12-02 14:48:30 +0100

    Updates ViscousHeatEquation application example

--- PacletInfo.m
+++ PacletInfo.m
@@ -2,7 +2,7 @@
 
 Paclet[
     Name -> "FEMAddOns",
-    Version -> "1.4.3",
+    Version -> "1.4.4",
     MathematicaVersion -> "11+",
     Description -> "Package provides additional finite element method functionality.",
     Extensions -> 
